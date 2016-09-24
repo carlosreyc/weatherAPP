fa2327c40e5b74db4367095d09f94d4a992c525a
charliekappa <sharlyebonham@gmail.com>
2016-09-24 04:43:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,36 @@ $(document).ready(function() {
   $("#getQuote").on("click", function(){
 
 
-    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en", function(json) {
-$("#quotes").html(JSON.stringify(json));
+    $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
+$("#message").html(json.quoteText + "<br> " + json.quoteAuthor);
+console.log(json.quoteText);
 });
 
 
   });
 
 });
+/*
+$(document).ready(function() {
+
+function parseQuote(response)
+{
+document.getElementById("quote").innerHTML = response.quoteText;
+document.getElementById("author").innerHTML = response.quoteAuthor;
+}
+
+
+
+  $("#getQuote").on("click", function(){
+
+//http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote&lang=en
+    $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
+$(".message").html(JSON.stringify(json));
+});
+
+
+  });
+
+});
+
+*/
