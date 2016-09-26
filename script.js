a598eda192bffdf43c141785e9466e7d6acf2611
charliekappa <sharlyebonham@gmail.com>
2016-09-25 19:33:43 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+
+  $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
+$("#quote").html(json.quoteText + "<footer>" + json.quoteAuthor + "</footer>");
+
+$("blockquote").css({"background-color": "#FFF", "border-left": "5px solid #CCC"});
+$("blockquote::before").css({"color": "#CCC"});
+});
+$.ajaxSetup({ cache: false });
   $("#getQuote").on("click", function(){
 
 
@@ -7,7 +15,9 @@ $(document).ready(function() {
 $("#quote").html(json.quoteText + "<br> " + json.quoteAuthor);
 $("blockquote").css({"background-color": "#FFF", "border-left": "5px solid #CCC"});
 $("blockquote::before").css({"color": "#CCC"});
+  /*$("#Tweet").on("click", function()) {
 
+  });*/
 $.ajaxSetup({ cache: false });
 });
 
