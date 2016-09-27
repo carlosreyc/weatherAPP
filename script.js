3e5572c4b68ec626104225509d1d6d360707d5d6
charliekappa <sharlyebonham@gmail.com>
2016-09-27 00:12:42 -0500

    WeatherAPP vr 0.01

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
+$(document).ready(function(){
 
-$(document).ready(function() {
+  $.getJSON('http://ipinfo.io', function(data){
+    var coords = data.loc.split(',');
+    var tempK = 0;
+    var tempC = 0;
+    console.log(coords)
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+coords[0]+'&lon='+coords[1]+'&APPID=d727f82db95a90f0f8909e62320acf9b&',function(json){
+      tempK = json.main.temp - 273.15;
+      tempC = Math.round( tempK * 10 ) / 10;
+      console.log(json);
+      console.log(tempC);
+      $("div.city").html(json.name +","+ json.sys.country);
+      $("div.weather").html(json.weather[0].description);
+      $("div.temp").html(tempC + 'C°');
 
-function changeColor(){
-var bg = ["#00ffab","#eaff7b","#d84242","#913f92","#b09563","#6c01de"];
-var rand = bg[Math.floor(Math.random() * bg.length)];
-$("body, #tweet-btn, #getQuote").css('background', rand);
-}
-
-$.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
-
-$("#quote").html(json.quoteText + "<footer>" + json.quoteAuthor + "</footer>");
-
-$("#tweet-btn").attr("href", "https://twitter.com/intent/tweet?text=" +json.quoteText+ "Author:"+json.quoteAuthor);
-  $.ajaxSetup({ cache: false });
-});
-
-
-$("#getQuote").on("click", function(){
-
-$.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
-  $("#quote").html(json.quoteText + "<footer>" + json.quoteAuthor + "</footer>");
-
-  changeColor();
-  $("#tweet-btn").attr("href", "https://twitter.com/intent/tweet?text=" +json.quoteText+ "Author:"+json.quoteAuthor);
-$.ajaxSetup({ cache: false });
-});
-
-
+    });
   });
+
 
 });
